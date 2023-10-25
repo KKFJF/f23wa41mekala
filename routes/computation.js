61f4252daab4a157c0932080901e2b6078a492af
deepthi mekala <143035056+KKFJF@users.noreply.github.com>
2023-10-25 15:49:42 -0500

    commit changes

--- routes/computation.js
+++ routes/computation.js
@@ -8,33 +8,21 @@ var router = express.Router();
  
 
 /* GET users listing. */
+var ran1 = Math.random() * 100;
+var ran2 = Math.random() * 200;
+var num1 = 0;
+var num2 = 0;
+var num3 = 0;
 
 router.get('/', function(req, res, next) {
 
-    x=0
-
-  if(req.query.x === undefined){
-
-    x = Math.random(7);
-
-  }else{
-
-    x = req.query.x;
-
-  }
-
  
+    num1 = Math.log(ran1);
+    num2 = Math.log(ran2);
+    num3 = Math.log(ran1);
 
-  imul = Math.imul(x);
-
-  log = Math.log(x);
-
-  log10 = Math.log10(x);
-
-  res.send("imul applied to " + x + " is " + imul+" log applied to" + x + " is " + log + "log10 applied to "+ x + " is "+ log10);
+    res.send(`The Random Numbers are ${ran1} and ${ran2} <br><br> imul applied to ${ran1} is ${num1} <br><br> log applied to ${ran2} is ${num2} <br><br> log10 applied to ${ran1} is ${num3}`);
 
 });
-
- 
 
 module.exports = router;
